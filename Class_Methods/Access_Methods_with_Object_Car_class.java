7ea66949a8920824208bde564c16d7f31dc6697c
Supansa Tantulset <s6201012620236@email.kmutnb.ac.th>
2020-08-17 00:55:46 +0700

    Class Methods Tutorial

---
+++ Class_Methods/Access_Methods_with_Object_Car_class.java
@@ -0,0 +1,25 @@
+// Create a Car class
+public class Access_Methods_with_Object_Car_class {
+ 
+  // Create a fullThrottle() method
+  public void fullThrottle() {
+    System.out.println("The car is going as fast as it can!");
+  }
+
+  // Create a speed() method and add a parameter
+  public void speed(int maxSpeed) {
+    System.out.println("Max speed is: " + maxSpeed);
+  }
+
+  // Inside main, call the methods on the myCar object
+  public static void main(String[] args) {
+    Access_Methods_with_Object_Car_class myCar = new Access_Methods_with_Object_Car_class();     // Create a myCar object
+    myCar.fullThrottle();      // Call the fullThrottle() method
+    myCar.speed(200);          // Call the speed() method
+  }
+}
+
+// The car is going as fast as it can!
+// Max speed is: 200
+
+
---
+++ Class_Methods/Class_Methods.java
@@ -0,0 +1,13 @@
+public class Class_Methods {
+  static void myMethod() {
+    // create a method named myMethod() in Class
+    System.out.println("Hello World!");  //print "Hello World!"
+  }
+
+  public static void main(String[] args) {
+    // call "myMethod" inside main
+    myMethod();
+  }
+}
+
+// Outputs "Hello World!"
