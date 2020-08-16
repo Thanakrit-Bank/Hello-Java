463b16b5b49d9ea839c632ed176f264df6ce871f
Thanakrit <s6201012620091@email.kmutnb.ac.th>
2020-08-16 23:11:08 +0700

    Java Booleans Tutorial

---
+++ JavaBooleans/Expression/Expression.java
@@ -0,0 +1,8 @@
+public class Expression {
+ public static void main (String[] args) {
+  int x = 10;
+  int y = 9;
+  System.out.println(x > y); // return true, because x = 10 is higher than y = 9
+ }
+}
+
---
+++ JavaBooleans/Values/Values.java
@@ -0,0 +1,9 @@
+public class Values {
+ public static void main(String[] args) {
+  boolean JavaFun = true;
+  boolean JavaNotFun = false;
+  System.out.println(JavaFun);
+  System.out.println(JavaNotFun);
+ }
+}
+
