846aef876fe44df7ce9543ef73bbec1cf9a6b839
Thanakrit <s6201012620091@email.kmutnb.ac.th>
2020-08-16 23:15:26 +0700

    Java If_Else Tutorial

---
+++ JavaIf_Else/Short_Hand_If_Else/Short_Hand_If_Else.java
@@ -0,0 +1,9 @@
+public class Short_Hand_If_Else {
+ public static void main(String[] args) {
+  int time = 20;
+  String result;
+  result = (time < 18) ? "Good day" : "Good evening";
+  System.out.println(result);
+ }
+}
+
