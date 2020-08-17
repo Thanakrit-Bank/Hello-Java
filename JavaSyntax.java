dcf7bec7b1ac865bde6f35007f2fe77f4e3bdb86
Phoka Pongpaet <s6201012630070@email.kmutnb.ac.th>
2020-08-18 00:45:48 +0700

    tell syntax of Java

---
+++ JavaSyntax.java
@@ -0,0 +1,7 @@
+//always start named the class with uppercase first letter
+// and name of the java file must match the class name 
+public class JavaSyntax {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
